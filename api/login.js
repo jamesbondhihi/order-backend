abf2bb16b53e7fcb51d55abef95f20154f087fed
jamesbondhihi <meinpojucktt@gmail.com>
2026-03-20 19:58:06 +0200

    Update login.js

--- api/login.js
+++ api/login.js
@@ -1,5 +1,5 @@
 const bcrypt = require('bcrypt');
-const pool = require('./db');
+const pool = require('../db');
 
 module.exports = async (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
